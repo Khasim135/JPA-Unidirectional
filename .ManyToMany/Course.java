c3f895824d7ed2d8a6309bb6c936849ccc276685
ZoroBasicCode <124671695+ZoroBasicCode@users.noreply.github.com>
2023-04-03 00:23:44 +0530

    Create Course.java

---
+++ .ManyToMany/Course.java
@@ -0,0 +1,36 @@
+package org.SRK;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int Duration;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDuration() {
+		return Duration;
+	}
+	public void setDuration(int duration) {
+		Duration = duration;
+	}
+	
+	
+}
